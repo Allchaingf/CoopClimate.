efcb5076edf9773c164538f98e80b58d7c201b08
anton0801 <danilovanton724@gmail.com>
2026-05-27 23:25:31 +0200

    Initial Commit

---
+++ Coop Climate/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Coop Climate
+//
+//  Created by Антон on 27/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Coop Climate/Coop_ClimateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coop_ClimateApp.swift
+//  Coop Climate
+//
+//  Created by Антон on 27/5/26.
+//
+
+import SwiftUI
+
+@main
+struct Coop_ClimateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
